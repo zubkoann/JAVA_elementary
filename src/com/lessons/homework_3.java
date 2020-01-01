028a875f4e21dae660a7460d0e8eda564dec4128
zubkoann <sultan62@ukr.net>
2020-01-01 22:46:28 +0200

    homework 3

---
+++ src/com/lessons/homework_3.java
@@ -0,0 +1,62 @@
+package com.lessons;
+
+import java.util.Scanner;
+
+public class homework_3 {
+    public static void main(String[] args) {
+        int number1 = getInt("first");
+        char operation = getOperation();
+        int number2 = getInt("second");
+        int result = calc(number1, number2, operation);
+        System.out.println("RESULT: " + result);
+    }
+    static Scanner s = new Scanner(System.in);
+    static int getInt(String num){
+        System.out.println("Enter " + num + " number");
+        int number;
+        if (s.hasNextInt()){
+            number = s.nextInt();
+        }else {
+            System.out.println("Something going wrong");
+            s.next();
+            number = getInt(num);
+        }
+        return  number;
+    }
+    static  char getOperation(){
+        System.out.println("Enter operation");
+        char operation;
+        if (s.hasNext()){
+            operation = s.next().charAt(0);
+        }else {
+            System.out.println("Something going wrong");
+            s.next();
+            operation = getOperation();
+        }
+        return  operation;
+    }
+    static  int calc(int number1, int number2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result =   number1 + number2;
+                break;
+            case '-':
+                result =   number1 - number2;
+                break;
+            case '/':
+                result =   number1 / number2;
+                break;
+            case '*':
+                result =   number1 * number2;
+                break;
+            case '%':
+                result =   number1 % number2;
+                break;
+            default:
+                System.out.println("Operation is wrong");
+                result = calc( number1,  number2, getOperation());
+        }
+        return result;
+    }
+}
