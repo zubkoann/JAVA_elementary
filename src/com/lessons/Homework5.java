693cfee01493d8254116971b947bb957d8bf1c00
zubkoann <sultan62@ukr.net>
2020-01-07 18:22:48 +0200

    Homework5

--- src/com/lessons/Homework5.java
+++ src/com/lessons/Homework5.java
@@ -5,23 +5,34 @@ import java.util.Scanner;
 public class Homework5 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String data;
-        int length;
-        boolean flag = false;
+        int number;
+        int count = 0;
         System.out.println("Please enter number");
-        data = s.nextLine();
-        length = data.length();
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                if (data.charAt(i) == data.charAt(j) && !flag && i != j) {
-                    flag = true;
-                    System.out.println("Tere is match ");
+        number = s.nextInt();
+        while (number>0 ){
+            int k = number%10;
+            number = number/10;
+            int l = number%10;
+            int number2 = number/10;
+
+            while (l>0){
+                if (k == l){
+                    count++;
                     break;
+
+                }else{
+                    l = number2%10;
+                    number2 = number2/10;
                 }
+
             }
-            if (flag) break;
+            if (count > 0) break;
+
         }
-        if (!flag)
-            System.out.println("Tere is NO match");
+        if (count > 0)
+            System.out.println("There is match ");
+        else
+            System.out.println("There is NO match");
+
     }
 }
