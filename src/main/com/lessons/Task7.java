1f2ccd2e2cdbf59d48d5a1ce2c3a90febb139691
zubkoann <sultan62@ukr.net>
2020-01-25 14:19:08 +0200

    getTask3

--- src/main/com/lessons/Task7.java
+++ src/main/com/lessons/Task7.java
@@ -1,11 +1,12 @@
 package com.lessons;
 
-import java.util.Arrays;
+import java.util.Random;
 
 public class Task7 {
     public static void main(String[] args) {
 //        getTask1();
         getTask2("i love java");
+        getTask3();
     }
 
     /**
@@ -35,17 +36,44 @@ public class Task7 {
     Входящий параметр:
     Строка со словами разделенными пробелом которые состоят из больших и маленьких букв.
     Метод должен вернуть ту же строку в которой первая буква в каждом слове большая, а остальные маленьк
+     * @return
      */
 
-    public static void getTask2(String str) {
+    public static String getTask2(String str) {
         String[] array =str.split("\\s");
         StringBuilder str2 = new StringBuilder();
         for (String word:array){
             String a = word.substring(0,1).toUpperCase();
-            String b = word.substring(1, word.length());
+            String b = word.substring(1, word.length()).toLowerCase();
             str2.append(a).append(b+ " ");
         }
-        System.out.println(str2);
+        return str2.toString().trim();
     }
 
+    /**
+     3) Создать генератор паролей, который будет генерировать случайные пароли по следующим правилам:
+     Пароль состоит из 8 символов
+     В пароле допускаются только латинские большие и маленькие буквы, цифры и символ подчеркивания
+     Пароль обязательно должен содержать Большие и маленькие буквы и цифры
+     */
+
+    public static String getTask3() {
+        StringBuilder str = new StringBuilder();
+        Random rand = new Random();
+        final int START_NUM = '0';
+        final int END_NUM = '9';
+        final int START_CHAR_BIG = 'A';
+        final int END_CHAR_BIG = 'Z';
+        final int START_CHAR_SM = 'a';
+        final int END_CHAR_SM = 'z';
+        final int UNDERSCOR = '_';
+        while (str.length()<9){
+            int num = rand.nextInt( 122-48+ 1) + 48;;
+            if ((num>=START_NUM && num<=END_NUM) || (num>=START_CHAR_BIG && num<=END_CHAR_BIG)
+                    ||(num>=START_CHAR_SM && num<=END_CHAR_SM) || num == UNDERSCOR){
+                str.append((char) num);
+            }
+        }
+        return str.toString();
+    }
 }
